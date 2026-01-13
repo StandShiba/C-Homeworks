c4d283ee53f982852ac493aaa18230ebf440b126
StandShiba <korneev092005@yandex.ru>
2026-01-13 13:48:44 +0300

    9

---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public int? Pace {  get; set; }
+        public Address Address { get; set; }
+
+    }
+}
